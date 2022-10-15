1c57050fed2f7ab79f303d0c4da2c191635982d2
Matheus Alefe <matheus.orvati@hotmail.com>
2022-10-14 22:20:05 -0300

    Include functions

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -3,36 +3,79 @@ import { Button } from "../Button";
 import { Container } from "./styles";
 
 export function Calculator(){
-    const [value, setValue] = useState('')
-    const [func, setFunction] = useState('')
-    const [result, setResult] = useState('')
+    const [value, setValue] = useState<string>()
+    const [secondValue, setSecondValue] = useState<string>()
+    const [func, setFunc] = useState<string>()
+    const [result, setResult] = useState<number>()
 
+    function SetValues(number:string){
+        !result? 
+            !func? 
+                !value? 
+                    setValue(number)
+                : 
+                    setValue(value.concat(number))
+            : 
+                !secondValue? 
+                    setSecondValue(number)
+                :
+                    setSecondValue(secondValue.concat(number))
+        :
+            (setResult(undefined), setValue(number))
+    }
 
+    function Calculate(){
+        if (func && value && secondValue){
+            if (func == 'sum'){
+                setResult(Number(value) + Number(secondValue))
+            } else if (func == 'min'){
+                setResult(Number(value) - Number(secondValue))
+            } else if (func == 'tim'){
+                setResult(Number(value) * Number(secondValue))
+            } else if (func == 'div'){
+                setResult(Number(value) / Number(secondValue))
+            } else if (func == 'pwr'){
+                setResult(Number(value) ** Number(secondValue))
+            } else if (func == 'root'){
+                setResult(Number(value) ** ( 1 / Number(secondValue)))
+            }
+            setFunc(undefined)
+        } else {
+            alert('Invalid expression')
+        }
+    }
+
+    function Clean(){
+        setValue(undefined)
+        setSecondValue(undefined)
+        setFunc(undefined)
+        setResult(0)
+    }
 
     return(
         <Container>
-            <div id="screen">{"Hello, world!"}</div>
+            <div id="screen">{result? result : secondValue? secondValue: func? func: value}</div>
             <section id="buttons">
                 <table id="numbers">
                     <tbody>
                         <tr>
-                            <td><Button>7</Button></td>
-                            <td><Button>8</Button></td>
-                            <td><Button>9</Button></td>
+                            <td><Button functionButton={() => SetValues('7')}>7</Button></td>
+                            <td><Button functionButton={() => SetValues('8')}>8</Button></td>
+                            <td><Button functionButton={() => SetValues('9')}>9</Button></td>
                         </tr>
                         <tr>
-                            <td><Button>4</Button></td>
-                            <td><Button>5</Button></td>
-                            <td><Button>6</Button></td>
+                            <td><Button functionButton={() => SetValues('4')}>4</Button></td>
+                            <td><Button functionButton={() => SetValues('5')}>5</Button></td>
+                            <td><Button functionButton={() => SetValues('6')}>6</Button></td>
                         </tr>
                         <tr>
-                            <td><Button>1</Button></td>
-                            <td><Button>2</Button></td>
-                            <td><Button>3</Button></td>
+                            <td><Button functionButton={() => SetValues('1')}>1</Button></td>
+                            <td><Button functionButton={() => SetValues('2')}>2</Button></td>
+                            <td><Button functionButton={() => SetValues('3')}>3</Button></td>
                         </tr>
                         <tr>
                             <td></td>
-                            <td><Button>0</Button></td>
+                            <td><Button functionButton={() => SetValues('0')}>0</Button></td>
                             <td></td>
                         </tr>
                     </tbody>
@@ -40,20 +83,20 @@ export function Calculator(){
                 <table id="functions">
                     <tbody>
                         <tr>
-                            <td><Button>+</Button></td>
-                            <td><Button>-</Button></td>
+                            <td><Button functionButton={() => setFunc('sum')}>+</Button></td>
+                            <td><Button functionButton={() => setFunc('min')}>-</Button></td>
                         </tr>
                         <tr>
-                            <td><Button>x</Button></td>
-                            <td><Button>^</Button></td>
+                            <td><Button functionButton={() => setFunc('tim')}>x</Button></td>
+                            <td><Button functionButton={() => setFunc('div')}>/</Button></td>
                         </tr>
                         <tr>
-                            <td><Button>/</Button></td>
-                            <td><Button>√</Button></td>
+                            <td><Button functionButton={() => setFunc('pwr')}>^</Button></td>
+                            <td><Button functionButton={() => setFunc('root')}>√</Button></td>
                         </tr>
                         <tr>
-                            <td><Button variant="red">C</Button></td>
-                            <td><Button variant="green">=</Button></td>
+                            <td><Button variant="red" functionButton={() => Clean()}>C</Button></td>
+                            <td><Button variant="green" functionButton={() => Calculate()}>=</Button></td>
                         </tr>
                     </tbody>
                 </table>
